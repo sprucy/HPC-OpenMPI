3b0294334d8ce4929fb1ce1df7d05b7e351dda67
sprucy <45484048+sprucy@users.noreply.github.com>
2023-01-28 13:56:46 +0100

    Add version 1.0 of parallel life game

---
+++ pmap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# --reservation=COURSE_RESERVATION_jhlsrf022
+#SBATCH --job-name="pmap"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=01:00:00
+#SBATCH --partition=normal
+#SBATCH --output=pmap_%j.out
+#SBATCH --error=pmap_%j.err
+
+module purge
+module load 2020
+module load GCC/9.3.0
+
+echo "OpenMP parallelism"
+echo
+
+for ncores in {1..16}
+do
+  export OMP_NUM_THREADS=$ncores
+
+  echo "CPUS: " $OMP_NUM_THREADS
+  echo "CPUS: " $OMP_NUM_THREADS >&2
+  ./pmap
+  echo "DONE "
+done
